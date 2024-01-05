ad59fe897e797f3b739be929b3ccbaf5b4517d61
Rababmedle <rabab.elmamy@gmail.com>
2024-01-05 19:09:20 +0100

    commit1

---
+++ BnidarkApp/src/main/java/com/example/BnidarkApp/Controllers/EmployeController.java
@@ -0,0 +1,41 @@
+package com.example.BnidarkApp.Controllers;
+
+import com.example.BnidarkApp.Entity.Employe;
+import com.example.BnidarkApp.Services.EmployeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeController {
+    private final EmployeService employeService;
+
+    public EmployeController(EmployeService employeService) {
+        this.employeService = employeService;
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+    @GetMapping("/emp")
+    public List<Employe> getAllEmployes() {
+        return employeService.getAllEmployes();
+    }
+
+    @GetMapping("/{id}")
+    public Employe getEmployeById(@PathVariable Integer id) {
+        return employeService.getEmployeById(id).orElse(null);
+    }
+
+    @PostMapping("/addemp")
+    public Employe saveEmploye(@RequestBody Employe employe) {
+        return employeService.saveEmploye(employe);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmploye(@PathVariable Integer id) {
+        employeService.deleteEmploye(id);
+    }
+}
